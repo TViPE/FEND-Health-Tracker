7d9f27fb6860c6bc40bbc0389d7b83b1fa1bb43a
TRI VI <trivi2710@gmail.com>
2017-04-25 16:08:06 -0400

    update FoodView with new tagName and deleteFoodItem function
    Chore: Update FoodView
    
    Update FoodView from list view to table view so changed tagName to
    ‘tr’. Added function deleteFoodItem when click on deleteBtn button of
    each food item. This button will remove the selected food item from the
    food collection and the view.

--- js/views/food.js
+++ js/views/food.js
@@ -2,11 +2,20 @@ var app = app || {};
 
 // Food View
 app.FoodView = Backbone.View.extend({
-	tagName: 'li',
+	tagName: 'tr',
 	template: _.template($('#food-template').html()),
 	
+	events: {
+		'click #deleteBtn' : 'deleteFoodItem'
+	},
+
 	render: function(){
 		this.$el.html(this.template(this.model.attributes));
 		return this;
+	},
+
+	deleteFoodItem : function(){
+		this.model.destroy();
 	}
+
 });
